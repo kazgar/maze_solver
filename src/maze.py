2ee9919e709c380ed21d06c906d74b74f90a21a7
kazgar <kazimierz.garstecki@gmail.com>
2024-10-19 10:45:46 +0200

    added maze class in maze.py

---
+++ src/maze.py
@@ -0,0 +1,35 @@
+from cell import Cell
+
+class Maze:
+    def __init__(
+            self,
+            x1,
+            y1,
+            num_rows,
+            num_cols,
+            cell_size_x,
+            cell_size_y,
+            win
+    ):
+        self.x1 = x1
+        self.y1 = y1
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.cell_size_x = cell_size_x
+        self.cell_size_y = cell_size_y
+        self.win = win
+        self._cells = []
+        self._create_cells()
+    
+    def _create_cells(self):
+        for i in range(self.num_cols):
+            self._cells.append([])
+            for j in range(self.num_rows):
+                self._cells[i].append(Cell())
+
+        for i in range(self.num_cols):
+            for j in range(self.num_rows):
+                self._draw_cell(self._cells[i][j], i, j)
+
+    def _draw_cell(self, i, j):
+        pass
