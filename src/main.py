defc4c860657078b6ed7c7a84cbfdd1cd1dc6c3b
kazgar <kazimierz.garstecki@gmail.com>
2024-10-19 13:05:38 +0200

    added _break_walls() in maze.py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 from maze_parts import Window, Point, Line
 from cell import Cell
 from maze import Maze
+import random
 
 def main():
-    num_rows = 12
-    num_cols = 16
+    num_rows = 8
+    num_cols = 8
     margin = 50
     screen_x = 800
     screen_y = 600
@@ -12,7 +13,8 @@ def main():
     cell_size_y = (screen_y - 2 * margin) / num_rows
     win = Window(screen_x, screen_y)
 
-    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win)
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win, 10)
+
     win.wait_for_close()
 
 if __name__ == "__main__":
--- src/maze.py
+++ src/maze.py
@@ -1,5 +1,6 @@
 from cell import Cell
 import time
+import random
 
 class Maze:
     def __init__(
@@ -10,7 +11,8 @@ class Maze:
             num_cols,
             cell_size_x,
             cell_size_y,
-            win=None
+            win=None,
+            seed=None
     ):
         self._x1 = x1
         self._y1 = y1
@@ -20,9 +22,12 @@ class Maze:
         self._cell_size_y = cell_size_y
         self._win = win
         self._cells = []
+        if seed:
+            random.seed(seed)
         self._create_cells()
         self._break_entrance_and_exit()
-        
+        self._break_walls(0, 0)
+       
     def _create_cells(self):
         if self._num_rows < 1 or self._num_cols < 1:
             raise ValueError("number of rows and number of columns have to be at least 1")
@@ -50,7 +55,7 @@ class Maze:
         if self._win is None:
             return
         self._win.redraw()
-        time.sleep(0.05)
+        time.sleep(0.005)
     
     def _break_entrance_and_exit(self):
         self._cells[0][0].has_top_wall = False
@@ -59,3 +64,33 @@ class Maze:
         self._cells[i][j].has_bottom_wall = False
         self._draw_cell(i, j)
         
+    def _break_walls(self, i, j):
+        self._cells[i][j]._visited = True
+        directions = [(0, -1), (0, 1), (-1, 0), (1, 0)]
+        while True:
+            to_visit = []
+            for di, dj in directions:
+                ni, nj = i + di, j + dj
+                if 0 <= ni < self._num_cols and 0 <= nj < self._num_rows and not self._cells[ni][nj]._visited:
+                    to_visit.append((ni, nj))
+
+            if not to_visit:
+                self._draw_cell(i, j)
+                return
+            
+            ni, nj = random.choice(to_visit)
+            if ni > i:
+                self._cells[i][j].has_right_wall = False
+                self._cells[ni][nj].has_left_wall = False
+            elif ni < i:
+                self._cells[i][j].has_left_wall = False
+                self._cells[ni][nj].has_right_wall = False
+            elif nj > j:
+                self._cells[i][j].has_bottom_wall = False
+                self._cells[ni][nj].has_top_wall = False
+            else:
+                self._cells[i][j].has_top_wall = False
+                self._cells[ni][nj].has_bottom_wall = False
+            
+            self._break_walls(ni, nj)
+
