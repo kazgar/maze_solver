94f5644fb31b68bcf2e22e1d3b3c39ae4552f771
kazgar <kazimierz.garstecki@gmail.com>
2024-10-19 11:04:11 +0200

    added _create_cells, _draw_cell and _animate in maze.py

--- src/maze.py
+++ src/maze.py
@@ -1,4 +1,5 @@
 from cell import Cell
+import time
 
 class Maze:
     def __init__(
@@ -11,25 +12,39 @@ class Maze:
             cell_size_y,
             win
     ):
-        self.x1 = x1
-        self.y1 = y1
-        self.num_rows = num_rows
-        self.num_cols = num_cols
-        self.cell_size_x = cell_size_x
-        self.cell_size_y = cell_size_y
-        self.win = win
+        self._x1 = x1
+        self._y1 = y1
+        self._num_rows = num_rows
+        self._num_cols = num_cols
+        self._cell_size_x = cell_size_x
+        self._cell_size_y = cell_size_y
+        self._win = win
         self._cells = []
         self._create_cells()
     
     def _create_cells(self):
-        for i in range(self.num_cols):
+        for i in range(self._num_cols):
             self._cells.append([])
-            for j in range(self.num_rows):
-                self._cells[i].append(Cell())
+            for j in range(self._num_rows):
+                self._cells[i].append(Cell(self._win))
 
-        for i in range(self.num_cols):
-            for j in range(self.num_rows):
-                self._draw_cell(self._cells[i][j], i, j)
+        for i in range(self._num_cols):
+            for j in range(self._num_rows):
+                self._draw_cell(i, j)
 
     def _draw_cell(self, i, j):
-        pass
+        if self._win is None:
+            return
+        cell = self._cells[i][j]
+        left_x = self._x1 + (j * self._cell_size_x)
+        right_x = left_x + self._cell_size_x
+        top_y = self._y1 + (i * self._cell_size_y)
+        bot_y = top_y + self._cell_size_y
+        cell.draw(left_x, top_y, right_x, bot_y)
+        self._animate()
+    
+    def _animate(self):
+        if self._win is None:
+            return
+        self._win.redraw()
+        time.sleep(0.05)
