c36d1c40f4cc0b73a39dc550d8a0eb9cd7cc32fd
kazgar <kazimierz.garstecki@gmail.com>
2024-10-19 10:31:20 +0200

    added draw_move() method in cell.py

--- src/cell.py
+++ src/cell.py
@@ -1,3 +1,4 @@
+from maze_parts import Line, Point
 
 class Cell:
     def __init__(self, win):
@@ -12,6 +13,8 @@ class Cell:
         self._win = win
         
     def draw(self, x1, y1, x2, y2):
+        if self._win is None:
+            return
         self._x1 = x1
         self._x2 = x2
         self._y1 = y1
@@ -29,3 +32,16 @@ class Cell:
             wall = Line(Point(x1, y2), Point(x2, self._y2))
             self._win.draw_line(wall)
 
+    def draw_move(self, to_cell, undo=False):
+        color = "red"
+        if undo:
+            color = "gray"
+
+        start_center_x = abs(self._x1 + self._x2) // 2
+        start_centery_y = abs(self._y1 + self._y2) // 2
+        
+        destination_center_x = abs(to_cell._x1 + to_cell._x2) // 2
+        destination_center_y = abs(to_cell._y1 + to_cell._y2) // 2
+
+        line = Line(Point(start_center_x, start_centery_y), Point(destination_center_x, destination_center_y))
+        self._win.draw_line(line, fill_color=color)
